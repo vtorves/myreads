da8a4e263c307527bed964af910656f3ec81e9dc
Vinicius Torves <vinicius@torves.com>
2018-04-28 18:24:54 -0300

    create component ListBook and create Links and Routes to SearchPage and ListBook

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,14 +1,22 @@
-
-  import React, { Component } from 'react'
+import React, { Component } from 'react'
+import ListBook from './ListBook'
+import { Link } from 'react-router-dom'
 
 class SearchPage extends Component {
+
   render() {
     return (
-        <div className="search-books">
+      <div className="search-books">
         <div className="search-books-bar">
-        <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-        <div className="search-books-input-wrapper">
-        {/*
+          <Link to="/"
+            component={ListBook}
+            className="close-search"
+          >Close
+          </Link>
+
+
+          <div className="search-books-input-wrapper">
+            {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -16,16 +24,16 @@ class SearchPage extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                   */}
-        <input type="text" placeholder="Search by title or author"/>
+                  <input type="text" placeholder="Search by title or author"/>
 
-        </div>
-        </div>
-        <div className="search-books-results">
-        <ol className="books-grid"></ol>
-        </div>
-        </div>
+                </div>
+              </div>
+              <div className="search-books-results">
+                <ol className="books-grid"></ol>
+              </div>
+            </div>
     );
   }
 }
 
-export default SearchPage 
+export default SearchPage
